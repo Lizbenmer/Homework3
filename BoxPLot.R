691efeb437b89da23a58679864d2295eb9ede047
Lizbenmer <lbenitezmerl@neiu.edu>
2025-10-09 02:45:19 +0000

    hey guys barchart added

---
+++ BoxPLot.R
@@ -0,0 +1,23 @@
+#This 
+setwd("/cloud/project")
+
+TextMessages <- read.csv("TextMessages.csv", header=TRUE)
+
+table(TextMessages$Group)
+
+install.packages("reshape")
+
+library(reshape)
+
+text <- melt(TextMessages, id=c("Participant","Group"), measured=c("Baseline","Six_months"))
+
+text$Group <- as.factor(text$Group)
+#install.packages("ggplot2")
+
+library(ggplot2)
+
+ggplot(text,aes(x=Group,y=value)) + geom_boxplot()
+
+
+
+
