0aaf57878a8e820e08bc71629c80fd2446f68e4f
Anabel1953 <abautista17@neiu.edu>
2025-10-21 00:15:11 +0000

    Hey

--- BoxPLot.R
+++ BoxPLot.R
@@ -18,6 +18,12 @@ library(ggplot2)
 
 ggplot(text,aes(x=Group,y=value)) + geom_boxplot()
 
+#Visually, the graph for participants in group 1 shows that the median is about
+#65. Also, we can see that there are two outliers below the minimum value. For 
+#group 2, the median seems to be about 68. Meanwhile, there are no outliers. 
+#Group 1 is left skewed, this means that participants fall mostly towards the
+#values below the median. Thus, for group 2, it is right skewed, so the value 
+#for the paricipant mostly goes above the median. 
 
 
 
