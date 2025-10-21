4172e5559fab63e624747002b15689f2b576cdf5
Lizbenmer <lbenitezmerl@neiu.edu>
2025-10-21 03:43:22 +0000

    Ladies,
    I added the Desc. Stats codes to my BoxPlot.R code starting with line 23

--- BoxPLot.R
+++ BoxPLot.R
@@ -20,9 +20,26 @@ ggplot(text,aes(x=Group,y=value)) + geom_boxplot()
 #NOTES: In Group 1, There are a couple of outliers. The data looks normally
 #distributed. My 25th percentile is about 53 a.....
 
-#Bar plot LBM  (9, 16 )
-ggplot(text, aes(x=value)) + geom_bar()
+###############Desc.Stats with RESHAPE data ###############
+#Install Pastecs package to run stats.
+install.packages("pastecs")
+library(pastecs)
+#
+
+#Here, I used the reshaped data "text" to obtain summary statistics on value 
+#by Group 
+by(text$value, text$Group, stat.desc)
+#This one is value by variable (time)
+by(text$value, text$variable, stat.desc)
+
+###############Desc. Stats with ORIGINAL data #############
+#In this process, I used the Original data "TextMessages" to obtain summary
+#statistics on Time (Baseline) by Group
+by(TextMessages$Baseline, TextMessages$Group, stat.desc)
+
+#Ran stats on Original data "TextMessages" to obtain summary statistics on
+#Time(Six_monts) by Group
+by(TextMessages$Six_months, TextMessages$Group, stat.desc)
 
 
-### Stats ###
 
